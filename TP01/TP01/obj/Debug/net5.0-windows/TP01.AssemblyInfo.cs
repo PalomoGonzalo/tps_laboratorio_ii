636327b2d5ce12432b2865f2b9fa76cf8865b625
Palamo Gonzalo <palomogonzalomatias@gmail.com>
2022-04-10 21:02:54 -0300

    creacion de tp01

--- TP01/TP01/obj/Debug/net5.0-windows/TP01.AssemblyInfo.cs
+++ TP01/TP01/obj/Debug/net5.0-windows/TP01.AssemblyInfo.cs
@@ -18,6 +18,8 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyProductAttribute("TP01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
+[assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
 
