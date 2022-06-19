829ad332755ad6f98835c052f428d100bf5c3763
PalomoGonzalo <89616271+PalomoGonzalo@users.noreply.github.com>
2022-06-19 17:53:36 -0300

    test unitarios terminado

---
+++ TP4/Pruebas/ManejadorDeDatosTest.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+
+namespace Pruebas
+{
+    [TestClass]
+    public class ManejadorDeDatosTest
+    {
+        [TestMethod]
+        public  void PasarStringNumeros_CuandoElStringEsEntero_DeberiaRetornarTrue()
+        {
+
+            string texto = "123";
+            bool resultadoEsperado = true;
+            bool resultado = ManejadorDeDatos.PasarStringNumeros(texto, out int numero);
+
+            Assert.IsTrue(resultado == resultadoEsperado);
+
+
+        }
+
+        [TestMethod]
+        public  void PasarStringNumeros_CuandoElStringEsUnTexto_DeberiaRetornarFalse()
+        {
+
+            string texto = "asdasd";
+            bool resultadoEsperado = false;
+            bool resultado = ManejadorDeDatos.PasarStringNumeros(texto, out int numero);
+            Assert.AreEqual(resultadoEsperado, resultado);
+
+        }
+
+    }
+}
